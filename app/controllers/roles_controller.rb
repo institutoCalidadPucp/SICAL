87231f5e7316cd50941cd683203cb412d188d213
jenisse <jenisse.vereau@pucp.pe>
2017-05-24 11:38:18 -0500

    se definieron las funciones create new edit update delete en roles_controller.rb

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,2 +1,45 @@
 class RolesController < ApplicationController
+
+	layout 'admin'
+
+	 before_action :set_role, only: [:edit, :update]
+
+	def index
+		@roles=Role.all
+	end
+
+	def new
+		@role=Role.new
+	end
+
+	def create
+		@role=Role.new role_params
+		if @role.save
+			redirect_to roles_path
+		else 
+			render :new
+		end
+ 	end
+
+ 	def edit
+
+ 	end
+
+ 	def update
+ 		@role.assign_attributes role_params
+ 		if @role.save
+			redirect_to roles_path, notice: 'Rol fue editado exitosamente'
+		else 
+			render :edit
+		end
+	end
+
+	private 
+	def role_params
+		params.require(:role).permit(:id,:name)
+	end
+
+	def set_role
+		@role=Role.find params[:id]
+	end
 end
