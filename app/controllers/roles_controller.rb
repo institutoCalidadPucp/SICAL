aa558a4e7dd3f07304ea88584faeff4fefcf2d9f
jenisse <jenisse.vereau@pucp.pe>
2017-05-24 14:34:29 -0500

    uso de espacios con width 2 sin layout en controller y params con nombre y descripcion

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,45 +1,43 @@
 class RolesController < ApplicationController
 
-	layout 'admin'
 
-	 before_action :set_role, only: [:edit, :update]
+  before_action :set_role, only: [:edit, :update]
+  def index
+    @roles=Role.all
+  end
 
-	def index
-		@roles=Role.all
-	end
+  def new
+    @role=Role.new
+  end
 
-	def new
-		@role=Role.new
-	end
+  def create
+    @role=Role.new role_params
+    if @role.save
+      redirect_to roles_path
+    else 
+      render :new
+    end
+  end
 
-	def create
-		@role=Role.new role_params
-		if @role.save
-			redirect_to roles_path
-		else 
-			render :new
-		end
- 	end
+  def edit
 
- 	def edit
+  end
 
- 	end
+  def update
+    @role.assign_attributes role_params
+    if @role.save
+      redirect_to roles_path, notice: 'Rol fue editado exitosamente'
+    else 
+      render :edit
+    end
+  end
 
- 	def update
- 		@role.assign_attributes role_params
- 		if @role.save
-			redirect_to roles_path, notice: 'Rol fue editado exitosamente'
-		else 
-			render :edit
-		end
-	end
+  private 
+  def role_params
+    params.require(:role).permit(:name,:description)
+  end
 
-	private 
-	def role_params
-		params.require(:role).permit(:id,:name)
-	end
-
-	def set_role
-		@role=Role.find params[:id]
-	end
+  def set_role
+    @role=Role.find params[:id]
+  end
 end
