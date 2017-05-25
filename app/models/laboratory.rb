257ad19e90955970fb50a20c92e3b6d152f24577
gustavoGalarreta <gustavo.galarreta@pucp.pe>
2017-05-24 23:49:13 -0500

    improve code review

--- app/models/laboratory.rb
+++ app/models/laboratory.rb
@@ -1,2 +1,6 @@
 class Laboratory < ApplicationRecord
+	validates :name, presence: true
+	validates :phone, length: { in: 7..9}
+	validates :phone, numericality: true
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
