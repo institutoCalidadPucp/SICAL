1ea8bad563f0c70b8e8d3416609f4d5280728bae
Owlz <jaume.bofi@gmail.com>
2017-05-29 12:58:43 -0500

    Fixing the migration and adding validation

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -1,2 +1,5 @@
-class Quotation < ApplicationRecord
-end
+class Quotation < ApplicationRecord 
+    validates :status, presence: true
+    validates :total, numeralicaty: { greater_than_or_equal_to => 0}
+    enum status: [:Pending,:Revised]
+end 
