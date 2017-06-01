03a5dbf444d2e9fac60ea30757e32ac737e6639f
gustavoGalarreta <gustavo.galarreta@pucp.pe>
2017-06-01 15:29:22 -0500

    dynamic tab samples

--- app/assets/javascripts/pages/samples.js
+++ app/assets/javascripts/pages/samples.js
@@ -1,3 +1,18 @@
+function removeCategoryTab(category_id) {
+  //category_content = $(".category");
+  //category_content.click(function(){
+  //  category_content.remove();
+  //});
+}
+
+function createCategoryTab(){
+  category_content =  document.getElementById('category-id')
+  category_content.insertAdjacentHTML('beforeend', '<div class="category" id="category-id"> <input id="category-content" type="text"> <span class="fa fa-plus" aria-hidden="true" ></span> <span class="fa fa-times" aria-hidden="true"></span></div>');
+  all_tabs = document.getElementById('all-tabs');
+  category_content.insertAdjacentHTML('beforeend', '<button class="tablinks" onclick="openData(event, Test)">Muestras</button>');
+}
+
+
 function openData(evt, tabName) {
       var i, tabcontent, tablinks;
       tabcontent = document.getElementsByClassName("tabcontent");
