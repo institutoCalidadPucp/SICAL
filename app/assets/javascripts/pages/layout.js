0303fbdc41392ac09174f5ab25a5fedc4c28cff3
Herbert V <herbert.videla@tektonlabs.com>
2017-06-01 18:24:59 -0500

    Turbolinks are dead, and a loader was add to all the views.

--- app/assets/javascripts/pages/layout.js
+++ app/assets/javascripts/pages/layout.js
@@ -1,14 +1,64 @@
 (function() {
+  function addDropdown(dropdown, dataName) {
+    dropdown.dropdown();
+    dropdown.attr(dataName, true);
+  }
+
+  function addTable(table, dataName) {
+    table.DataTable({
+      "pagingType": "full_numbers",
+      "lengthMenu": [ 
+        [10, 25, 50, -1],
+        [10, 25, 50, "All"]
+      ],
+      responsive: true,
+      destroy: true,
+      language: {
+        search: "_INPUT_",
+        searchPlaceholder: "Buscar Laboratorios",
+      },
+    });
+
+    table.attr(dataName, true);
+  }
+
+  function initializeElements(index, element, name) {
+    var $element = $(element);
+    var dataName = name + '-attached';
+
+    if (!$element.attr(dataName)) {
+      switch(name) {
+        case 'dropdown':
+          addDropdown($element, dataName);
+          break;
+        case 'table':
+          addTable($element, dataName);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
   $(function() {
     var $dropdowns = $('.sical-dropdown');
-    
-    $dropdowns.each(function (index, dropdown) {
-      var $dropdown = $(dropdown);
+    var $tables = $('.sical-data-table');
+    var $loader = $('#loader');
+    var $content = $('#content');
 
-      if (!$dropdown.data('dropdown-attached')) {
-        $dropdown.dropdown();
-        $dropdown.data('datepicker-attached', true);
-      }
+    $dropdowns.each(function(index, element) {
+      initializeElements(index, element, 'dropdown');
     });
-  })
+
+    $tables.each(function(index, element) {
+      console.log(index, element);
+      initializeElements(index, element, 'table');
+    });
+
+    setTimeout(function () {
+      $loader.addClass('hidden');
+      $content.removeClass('hidden');
+    }, 500);
+  });
+
 })();
