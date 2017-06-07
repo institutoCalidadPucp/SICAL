58bd19eb08ac811f73a2b160da032aae474b089b
FallenPotatoChip <a20134674@pucp.pe>
2017-06-07 18:18:41 -0500

    Fixing Date Of Birth Employees

--- db/migrate/20170525054330_add_user_information_for_employee.rb
+++ db/migrate/20170525054330_add_user_information_for_employee.rb
@@ -1,7 +1,7 @@
 class AddUserInformationForEmployee < ActiveRecord::Migration[5.1]
   def change
   	add_column :users, :last_name, :string
-  	add_column :users, :date_of_birth, :date
+  	add_column :users, :date_of_birth, :datetime
   	add_column :users, :gender, :integer
   end
 end
