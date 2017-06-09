3d5510ec87b66c3dfddc9425777bd763dc944b1e
Lourdes <montalvo.lourdes@pucp.edu.pe>
2017-06-09 14:27:48 -0500

    index sample with lock n unlock

--- app/assets/javascripts/pages/samples.js
+++ app/assets/javascripts/pages/samples.js
@@ -10,6 +10,26 @@
       $this.on('keypress',onlyNumbers);
     })
   })
+
+  $(".removeBtn").click(function(){
+    $(this).parent('button').next('div').andSelf().remove();
+  });
+
+  $(function(){
+    var $accordions = $(".accordion");
+    $accordions.each(function (accordion) {
+      var $accordion = $(accordion);
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+          panel.style.display = "none";
+      } else {
+          panel.style.display = "block";
+      }
+    })
+  })
+  
+
 })();
 
 
