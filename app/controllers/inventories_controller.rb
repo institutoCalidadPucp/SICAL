bb1224b723de22f8391648589274ed4f145450dc
jenisse <jenisse.vereau@pucp.pe>
2017-06-10 18:33:17 -0500

    se resolvio el error

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -28,7 +28,7 @@ class InventoriesController < ApplicationController
   def update
     @inventory.assign_attributes inventory_params
     if @inventory.save
-      redirect to inventories_path
+      redirect_to inventories_path
     else 
       render :edit
     end
