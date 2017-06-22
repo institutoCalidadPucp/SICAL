ac037effb184ce80b9a00f465bd4f0a65bf7ceb4
Herbert V <herbert.videla@tektonlabs.com>
2017-06-22 17:06:53 -0500

    little fix

--- app/models/sample_method.rb
+++ app/models/sample_method.rb
@@ -5,9 +5,10 @@ class SampleMethod < ApplicationRecord
   validates :unit_cost, numericality: { :greater_than_or_equal_to => 0 }
 
   has_many :sample_processeds
-  belongs_to :laboratory  
+  belongs_to :laboratory
+
   enum status: [:active, :inactive]
-  enum accreditation: [:accredited, :non_accredited]  
+  enum accreditation: [:accredited, :non_accredited]
 
   def self.own_per_user current_user
     if current_user.admin?
