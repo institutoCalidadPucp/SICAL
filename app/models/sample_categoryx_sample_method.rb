0ed4353c53f8d2ebba96cbb94fc13cbf8633ed56
Owlz <jaume.bofi@gmail.com>
2017-06-29 11:28:38 -0500

    View side of the new Sample Category

--- app/models/sample_categoryx_sample_method.rb
+++ app/models/sample_categoryx_sample_method.rb
@@ -1,4 +1,6 @@
 class SampleCategoryxSampleMethod < ApplicationRecord
 	belongs_to :sample_category
 	belongs_to :sample_method
+	has_many :chain_features
+	accepts_nested_attributes_for :chain_features, :allow_destroy => true, :reject_if => :all_blank
 end
