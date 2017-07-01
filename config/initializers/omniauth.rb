fd6b82bf7e38b7243f0f38147e9781cc29dc1f1a
kervyRivas <k.rivas@pucp.pe>
2017-07-01 11:15:12 -0500

    fix error jws.iat

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, 
     GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, 
-    {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+    {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}, skip_jwt: true
 end
