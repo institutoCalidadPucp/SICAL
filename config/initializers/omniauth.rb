716ea7efea62731d07fb2b44242e7acdb1aa81c1
Bach <carlos.bachmann@pucp.pe>
2017-07-01 11:47:20 -0500

    fix cacert.pem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, 
-    GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, 
-    {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}, skip_jwt: true
+    GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, skip_jwt: true
 end
