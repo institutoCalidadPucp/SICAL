b5b78f00d5a1325edfd121a7e64abbd31a778083
HerbertVidela <hvidelac@gmail.com>
2017-07-01 17:49:51 -0500

    only one partial with optioins in work_classified_services

--- app/assets/javascripts/pages/services.js
+++ app/assets/javascripts/pages/services.js
@@ -14,5 +14,33 @@ function openSection(evt, tabFeature) {
 
 
 (function() {
+  $(function() {
+    var $btnEvaluationSample = $('.btn-add-evalution-sample');
+    var $sampleEvaluations = $('.sample-evaluations');
 
+    $btnEvaluationSample.on('click', function() {
+      var counter = $(this).data('evaluation-samples');
+      
+      counter += 1;
+
+      $(this).data('evaluation-samples', counter);
+    });
+
+
+    $sampleEvaluations.on('cocoon:before-insert', function(e, insertedItem) {
+      var counter = $btnEvaluationSample.data('evaluation-samples');
+      var itemId = 'samples-values-' + counter;
+
+      $(insertedItem[0]).find('.samples-values').attr('id', itemId);
+    });
+
+    $sampleEvaluations.on('cocoon:after-insert', function(e, insertedItem) {
+      $('.btn-add-sample-value').data('association-insertion-node', function(link) {
+        var counter = $btnEvaluationSample.data('evaluation-samples');
+        var itemId = '#samples-values-' + counter;
+
+        return $(itemId);
+      });
+    });
+  })
 })()
