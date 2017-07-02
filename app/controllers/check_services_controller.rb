bc6903ea7412d77f1323a9755692d739dec8d490
jose_amz <jose.amador@pucp.pe>
2017-07-01 19:15:10 -0500

    finished filtering services by owner

--- app/controllers/check_services_controller.rb
+++ app/controllers/check_services_controller.rb
@@ -7,7 +7,8 @@ before_action :set_service, only: [:show]
   end  
 
   def search
-  	@services = Service.where "created_at >= :start_date AND created_at <= :end_date", {start_date: params[:start_date].to_date, end_date: params[:end_date].to_date}   
+    user_services = Service.own_per_user current_user    
+  	@services = user_services.where "created_at >= :start_date AND created_at <= :end_date", {start_date: params[:start_date].to_date, end_date: params[:end_date].to_date}   
   end
 
   private 
