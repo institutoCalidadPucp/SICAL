241b3ef643286bdb53d6fb3063fa0644d1256d35
HerbertVidela <hvidelac@gmail.com>
2017-08-08 07:22:23 -0500

    Add styles and validations to the contact us form
    The necesary classes to validate and give styles to the form
    were added. Also the email should now display the message
    written in the form.

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -1,25 +1,25 @@
 class ContactUsController < ApplicationController
-	before_action :set_laboratories, only: [:new, :create]
+  before_action :set_laboratories, only: [:new, :create]
 
-	def new
-		@contact_us = ContactU.new
-	end
+  def new
+    @contact_us = ContactU.new
+  end
 
-	def create
-		@contact_us = ContactU.new contact_us_params
-		if @contact_us.save
-			ContactUsMailer.contact_email(@contact_us).deliver_now
+  def create
+    @contact_us = ContactU.new contact_us_params
+    if @contact_us.save
+      ContactUsMailer.contact_email(@contact_us).deliver_now
       redirect_to new_contact_u_path
-		end
-	end
+    end
+  end
 
-	private
+  private
 
     def contact_us_params
       params.require(:contact_u).permit(:subject, :message, :laboratory_id)
-    end	
+    end
 
     def set_laboratories
-    	@laboratories = Laboratory.all
+      @laboratories = Laboratory.all
     end
 end
